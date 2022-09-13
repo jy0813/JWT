d0f7b15f92510021e3f8ab652467378d3466e373
jy0813 <busbug3@gmail.com>
2022-09-13 13:07:41 +0900

    feat: login 및 jwt 발행

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user"); // 위에서 설계한 User 정보를 담기위한 document 모델
 const bcrypt = require("bcryptjs"); // 유저가 입력한 password 를 암호화 하기 위한 알고리즘 라이브러리
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.SECRET_KEY;
 
 const createUserData = async (userInput) => {
   // user를 객체화하고 Database에 저장하는 함수
@@ -39,4 +41,28 @@ const signUp = async (req, res, next) => {
   }
 };
 
-module.exports = { signUp }; // signUp 함수를 module 로 내보낸다.
+const createToken = (userId) => {
+  const token = jwt.sign({ _id: userId.toString() }, secretKey); // 인자로 넘겨받은 user 도큐먼트(객체)의 고유 id로 토큰을 만든다. 두번째 인자값은 salt 값으로 보안과 관련된 값이므로, 보통 dotenv 로 환경변수에 넣어서 사용하게 된다
+  return token;
+};
+
+const signIn = async (req, res, next) => {
+  try {
+    const { email = null, password = null } = req.body; // POST 메소드로 들어온 요청의 데이터(body)에서 email, password 를 destructuring 한다.
+    if (!email || !password) errorGenerator("Invalid inputs", 400); // input 으로 들어오지 않은 경우에 잘못된 인풋이라는 에러를 던진다.
+
+    const user = await User.findOne({ email }); //email로 조회
+    if (!user) errorGenerator("User not found", 404); // user 가 없을 경우에 error 를 발생시킨다.
+
+    const passwordCheck = await bcrypt.compare(password, user.password); // bcrypt compare 함수로 입력된 비밀번호와 암호화되어서 저장된 비밀번호를 비교한다.
+    if (!passwordCheck) errorGenerator("Wrong password", 404); // 암호가 같지 않을 경우에 잘못된 패스워드라는 메시지로 에러를 던진다.
+
+    const token = createToken(user._id); // user 도큐먼트(객체)의 고유한 id로 토큰을 만든다.
+
+    res.status(201).json({ message: "success", token }); // token 을 response로 넘겨준다.
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { signUp, signIn }; // signUp, signIn 함수를 module 로 내보낸다.
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { signUp } = require("../controllers/auth");
+const { signUp, signIn } = require("../controllers/auth");
 
 router.post("/signup", signUp); //signup 이라는 미들웨어를 signup 의 주소와 연결 시켜준다. 즉 front-end 클라이언트서버는 홈페이지주소/auth/signup 으로 회원가입 요청을 보낼 수 있게 된다.
+router.post("/signin", signIn);
 module.exports = rotuer;
